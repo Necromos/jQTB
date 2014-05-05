e2091c1cb36b134b776cde27c2502cd9b3931b54
Necromos <masternecromos@gmail.com>
2014-05-05 06:15:24 +0000

    background for other data format

--- src/jQTB.js
+++ src/jQTB.js
@@ -8,18 +8,19 @@
 (function($){
     "use strict";
     var data, numberOfTeams, options;
+    var createFlag = false;
 
     var log2 = function (val) {
         return Math.log(val)/Math.LN2;
     };
     
-    var createBrackets = function (mainContainer) {
+    var createBrackets = function (that, callback) {
         data = options.data;
         numberOfTeams = data.teams.length;
         var i;
         for ( i = 1 ; i <= log2(numberOfTeams) + 1 ; i++ ) {
             var bracketsContainer = $(document.createElement('div')).addClass('bracketsContainer');
-            $(mainContainer).append(bracketsContainer);
+            $(that).append(bracketsContainer);
             for ( var j = 0 ; j < Math.floor( numberOfTeams / i ) ; j++ ) {
                 var bracket = $(document.createElement('div')).addClass('bracket');
                 if( i == 1 ) {
@@ -28,23 +29,63 @@
                 bracketsContainer.append(bracket);
             }
         }
-        fillResults(mainContainer);
+        if(callback)
+            callback(that)
     };
 
-    var fillResults = function (mainContainer) {
+    var fillResults = function (that) {
         var cursor = 0;
-        $(mainContainer).children('.bracketsContainer').each(function(bracketContainerIndex, bracketContainer) {
-            $(bracketContainer).children('.bracket').each(function(bracketIndex, bracket) {
+        $(that).children('.bracketsContainer').each( function (bracketContainerIndex, bracketContainer) {
+            $(bracketContainer).children('.bracket').each( function (bracketIndex, bracket) {
                 var currResult = data.results[cursor][bracketIndex%2];
                 var textToDisplay = currResult.teamName + ' ';
                 textToDisplay += currResult.points === undefined ? '' : currResult.points;
                 $(bracket).text(textToDisplay);
                 if (bracketIndex % 2 == 1)
                     cursor++;
+                // need special case of function for odd number of brackets
             });
         });
     };
     
+    var prepareAutoFill = function () {
+        var results = options.data.results;
+        var newResults = [];
+        $.each(results, function (index, value) {
+            var tmpObject = {teamName: , points:}
+        });
+        options.data.results = results;
+    };
+    
+    /*
+    var autoFillResults = function (that) {
+        var mainCursor = 0;
+        var detailCursor = 0;
+        $(that).children('.bracketsContainer').each( function (bracketContainerIndex, bracketContainer ) {
+            $(bracketContainer).children('.bracket').each( function (bracketIndex, bracket) {
+                var currTeam;
+                if (data.teams[mainCursor][detailCursor])
+                    currTeam = data.teams[mainCursor][detailCursor];
+                else {
+                    var prevResults = data.results[bracketIndex];
+                    if ( prevResults[0] > prevResults[1] )
+                        currTeam = data.teams[bracketIndex][0];
+                    else
+                        currTeam = data.teams[bracketIndex][1];
+                }
+                var textToDisplay = currTeam;
+                textToDisplay += currTeam + data.results[mainCursor][detailCursor];
+                if (bracketIndex % 2 == 1)
+                    mainCursor++;
+                if ( detailCursor === 0 )
+                    detailCursor++;
+                else
+                    detailCursor--;
+                // need special case of function for odd number of brackets
+            });
+        });
+    };
+    */
     //to be implemented
     // var callData = function (that) {
     //     $.getJSON(options.url)
@@ -57,25 +98,48 @@
     // };
     
     var methods = {
-        init: function (userOptions){
+        init: function () {
             options = $.extend({}, $.fn.bracket.defults, userOptions);
             // if (options.url) {
             //     callData(this);
             // }
             // else {
-                createBrackets(this);
+            createBrackets(this, fillResults);
             // }
+            return this;
+        },
+        create: function () {
+            createBrackets(this);
+            createFlag = true;
+            return this;
+        },
+        fill: function () {
+            if (createFlag)
+                fillResults(this);
+            else
+                $.error( 'Method fill requires first to create brackets. Run .bracket(\'create\') first.' );
+            return this;
+        },
+        autoFill: function () {
+            if (createFlag)
+                autoFillResults(this);
+            else
+                $.error( 'Method autoFill requires first to create brackets. Run .bracket(\'create\') first.' );
+            return this;
+        },
+        setup: function (userOptions) {
+            options = $.extend({}, $.fn.bracket.defults, userOptions);
             return this;
         }
     };
     
-    $.fn.bracket = function (methodOrOptions){
+    $.fn.bracket = function (method){
         if ( methods[methodOrOptions] ) {
-            return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-        } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
+            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || !method ) {
             return methods.init.apply( this, arguments );
         } else {
-            $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.bracket' );
+            $.error( 'Method ' +  method + ' does not exist on jQuery.bracket' );
         }    
     };
 
@@ -84,6 +148,12 @@
             teams: ['teamA','teamB','teamC','teamD'],
             results: [ [{teamName: 'teamA', points: 2},{teamName: 'teamB', points: 3}], [{teamName: 'teamC', points: 5},{teamName: 'teamD', points: 2}], [{teamName: 'teamB', points: 3},{teamName: 'teamC', points: 2}], [{teamName: 'teamB'}]]
         }
+        /* //data sample for autofill
+        data: {
+            teams: [ 'teamA', 'teamB', 'teamC', 'teamD' ], //as matchups go
+            results: [ [1, 2], [2, 1], [3, 1] ]  //keep it simple
+        }
+        */
     };
 
 }( jQuery ));
